9e3431c38eac0e714d2ba40f75cd47f6fa45b69e
cs-locx <oskari.johansson@helsinki.fi>
2014-01-15 22:24:59 +0200

    nb-projekti lisätty

---
+++ tarjousfeed/src/main/java/ohha/tarjousfeed/App.java
@@ -0,0 +1,13 @@
+package ohha.tarjousfeed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
